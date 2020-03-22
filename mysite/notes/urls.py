12e02830a05a6e2e4064e1f5f27bebc44e888f10
tkong233 <tkong@wesleyan.edu>
2020-03-22 13:57:56 -0400

    handled add new notes

--- mysite/notes/urls.py
+++ mysite/notes/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'notes'
 urlpatterns = [
+    # ex: /notes/
     path('', views.index, name='index'),
+    # ex: /notes/add/
+    path('add/', views.add, name='add'),
 ]
--- mysite/notes/views.py
+++ mysite/notes/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Note
 
 def index(request):
     notes = Note.objects.all()
     context = {'notes': notes}
     return render(request, 'notes/index.html', context)
+
+def add(request):
+    try:
+        new_note_text = request.POST['new_note']
+    except KeyError:
+        raise("No note to add")
+    else:
+        n = Note(text=new_note_text)
+        n.save()
+        return HttpResponseRedirect(reverse('notes:index'))
