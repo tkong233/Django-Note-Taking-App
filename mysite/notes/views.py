ce549d0d1326fa95f155b9c65ea35f3d0851d603
tkong233 <tkong@wesleyan.edu>
2020-03-22 12:30:32 -0400

    created index view to render notes

--- mysite/notes/views.py
+++ mysite/notes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Note
 
 def index(request):
-    return HttpResponse("Hello World, you are at the notes index.")
+    notes = Note.objects.all()
+    context = {'notes': notes}
+    return render(request, 'notes/index.html', context)
